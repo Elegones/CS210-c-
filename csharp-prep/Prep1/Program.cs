0398f7dbf2242e429ee34fd02ba3419aa3728a15
Elegones <ethan.legones@gmail.com>
2026-04-24 16:36:14 -0600

    Prep1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+       string first= Console.ReadLine();
+
+        Console.Write("what is your Last name? ");
+       string Last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Last}, {first} {Last}.");
+
     }
 }

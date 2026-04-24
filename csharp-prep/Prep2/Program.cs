39308277fb32c6d11e4e1932b4cfc02d291bd5c7
Elegones <ethan.legones@gmail.com>
2026-04-24 17:12:52 -0600

    Prep 2 complited

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Hello what percetage grade did you get? ");
+        string gradeString =Console.ReadLine();
+        int gradeNum= int.Parse(gradeString);
+        string letterGrade;
+
+        if(gradeNum>=90)
+        {
+            letterGrade="A";
+        }
+        else if(gradeNum>=80)
+        {
+            letterGrade="B";
+        }
+         else if(gradeNum>=70)
+        {
+            letterGrade="C";
+        }
+        else if(gradeNum>=60)
+        {
+            letterGrade="D";
+        }
+        else
+        {
+            letterGrade="F";
+        }
+        Console.Write($"Your grade is A {letterGrade} ");
+
+        if(gradeNum>=70)
+        {
+            Console.WriteLine("You passed.");
+        }
+        else 
+        {
+            Console.WriteLine("You failed, better luck next time.");
+        }
+    
     }
 }

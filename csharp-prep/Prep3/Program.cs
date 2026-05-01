3bb270469ddfa4905d4ecebccae7cfe5b8dbe7ed
Elegones <ethan.legones@gmail.com>
2026-05-01 00:49:36 -0600

    prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNum =  randomGenerator.Next(1,100);
+        int guess=0;
+        string guessWord;
+        Console.WriteLine("Guess a number");
+        while(guess!=magicNum)
+        {
+           guessWord= Console.ReadLine();
+           guess=int.Parse(guessWord);
+
+           if(guess>magicNum)
+            {
+                Console.WriteLine("too high, guess again");
+
+            }
+            else if(guess<magicNum)
+            {
+               Console.WriteLine("too low, guess again"); 
+
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+            }
+
+
+
+        }
+
+        
+
+
     }
 }

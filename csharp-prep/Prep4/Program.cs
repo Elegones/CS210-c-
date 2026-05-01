ddee6c9795473a6bb43736c926fdd20a10f7fa34
Elegones <ethan.legones@gmail.com>
2026-05-01 11:56:21 -0600

    Prep 4 completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int total=0;
+        int max=0;
+        
+        int listSize=0;
+        List<int> numbers= new List<int>();
+        string numberIn;
+        int math=1;
+        Console.WriteLine("Enter a list of numbers and 0 to end it.");
+        while(math!=0)
+        {
+           numberIn = Console.ReadLine();
+           math=int.Parse(numberIn);
+           numbers.Add(math);
+           Console.WriteLine("Enter a number");
+        }
+
+        foreach(int num in numbers)
+        {
+            total= total +num;
+            if(max<num)
+            {
+                max=num;
+            }
+        }
+        listSize= numbers.Count -1;
+       decimal ave =(decimal)total/listSize;
+        Console.WriteLine($"Sum of numbers is {total}");
+        Console.WriteLine($"Largest number is {max}");
+        Console.WriteLine($"The list average is  {ave}");
     }
 }
+

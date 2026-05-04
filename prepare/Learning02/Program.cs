fe26f210292e22dad9d9c6d59b5626649c66a8d6
Elegones <ethan.legones@gmail.com>
2026-05-03 19:33:11 -0600

    Learning 2 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+       Job job1= new Job();
+       job1._jobTitle=" Aprentace Electrical Engineer";
+       job1._company="PNL";
+       job1._startYear= 2009;
+       job1._endYear=2015;
+
+
+       Job job2= new Job();
+       job2._jobTitle="Master Electrical Engineer";
+       job2._company="Lockhead Martin ";
+       job2._startYear= 2015;
+       job2._endYear=2025;
+
+      Resume r1 =new Resume();
+      r1._name="Ethan Legones";
+      r1._jobs.Add(job1);
+      r1._jobs.Add(job2);
+
+      r1.DisplayResume();
     }
 }

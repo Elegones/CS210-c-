534d249e449c89040e2ee1e0a2d2c5db5b5d6ab4
Elegones <ethan.legones@gmail.com>
2026-06-17 22:47:20 -0600

    practice

---
+++ sandbox/Animal/Dog.cs
@@ -0,0 +1,14 @@
+
+
+class Dog: Animal
+{
+    public Dog(string name):base(name)
+    {
+        
+    }
+public override void MakeNoise()
+    {
+        Console.WriteLine($"{_name} says bark");
+    }
+
+}
---
+++ sandbox/Animal/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main()
+    {
+
+        List<Animal> myAnimals=new List<Animal>();
+        myAnimals.Add( new Animal("Liger"));
+        myAnimals.Add( new Dog("pepper"));
+
+        foreach (Animal critter in myAnimals)
+        {
+         critter.MakeNoise();
+        }
+     
+    }
+
+
+}

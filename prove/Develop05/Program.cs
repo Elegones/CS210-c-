1a03a56170892559df7f1e6a4d5d90877a4669c6
Elegones <ethan.legones@gmail.com>
2026-07-01 13:48:16 -0600

    working on main

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+     int points;
+     List<Goal> tasks=new List<Goal>();
+     int choice=0;
+     
+     while(choice!=6)
+        {
+      Console.WriteLine("Menu:");
+      Console.WriteLine(" 1: List Goals");
+      Console.WriteLine(" 2: Add goal");
+      Console.WriteLine(" 3: Record Event");
+      Console.WriteLine(" 4: Load Goals");
+      Console.WriteLine(" 5: save Goals");
+      Console.WriteLine(" 6: Exit");
+      choice=int.Parse(Console.ReadLine());
+
+      if(choice==1)
+            {
+                foreach(Goal goal in tasks)
+                {
+                    goal.Display();
+
+                }
+
+            }
+      
+        }
+
+      if(choice==2)
+        {
+            Console.WriteLine("what type of goal do you want to add?");
+            
+        }
+
+
     }
 }
